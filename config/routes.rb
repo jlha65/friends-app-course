22e57b94f2b9cee4f372ff887b2461fa4c392f2b
Jose Luis Herrera <jose-luis1997@hotmail.com>
2022-02-11 16:48:14 -0700

    First commit (curso)

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :friends
+  get 'home/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'home/about'
+
+  # Defines the root path route ("/")
+  #root "home#index"
+  root "friends#index"
+end
